c265b5ad9f55ce200eb0d98f2fdb195a5bf8391a
Christos Ploutarchou <cploutarchou@gmail.com>
2023-04-08 09:57:19 +0300

    Update ratelimit test

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -3,13 +3,14 @@ package ratelimiter
 import (
 	"context"
 	"fmt"
-	"github.com/ory/dockertest"
-	"github.com/redis/go-redis/v9"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
+
+	"github.com/ory/dockertest"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRateLimiter_Limit(t *testing.T) {
@@ -17,15 +18,15 @@ func TestRateLimiter_Limit(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not connect to Docker: %s", err)
 	}
-	
+
 	resource, err := pool.Run("redis", "6", nil)
 	if err != nil {
 		t.Fatalf("Could not start resource: %s", err)
 	}
 	defer pool.Purge(resource)
-	
+
 	var client *redis.Client
-	
+
 	if err := pool.Retry(func() error {
 		client = redis.NewClient(&redis.Options{
 			Addr: fmt.Sprintf("localhost:%s", resource.GetPort("6379/tcp")),
@@ -35,36 +36,36 @@ func TestRateLimiter_Limit(t *testing.T) {
 		t.Fatalf("Could not connect to Docker: %s", err)
 	}
 	defer client.Close()
-	
+
 	rl := NewRateLimiter(client, 2, time.Minute)
-	
+
 	testHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "test response")
 	})
-	
+
 	limitedHandler := rl.Limit(testHandler)
-	
+
 	rr := httptest.NewRecorder()
-	
+
 	req, err := http.NewRequest(http.MethodGet, "/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-	
+
 	// First two requests should be allowed
 	limitedHandler.ServeHTTP(rr, req)
 	assert.Equal(t, http.StatusOK, rr.Code)
 	assert.Equal(t, "test response\n", rr.Body.String())
 	assert.Equal(t, "2", rr.Header().Get("X-RateLimit-Limit"))
 	assert.Equal(t, "1", rr.Header().Get("X-RateLimit-Remaining"))
-	
+
 	rr = httptest.NewRecorder()
 	limitedHandler.ServeHTTP(rr, req)
 	assert.Equal(t, http.StatusOK, rr.Code)
 	assert.Equal(t, "test response\n", rr.Body.String())
 	assert.Equal(t, "2", rr.Header().Get("X-RateLimit-Limit"))
 	assert.Equal(t, "0", rr.Header().Get("X-RateLimit-Remaining"))
-	
+
 	// Third request should be rejected
 	rr = httptest.NewRecorder()
 	limitedHandler.ServeHTTP(rr, req)
@@ -72,5 +73,5 @@ func TestRateLimiter_Limit(t *testing.T) {
 	assert.Equal(t, "Too many requests\n", rr.Body.String())
 	assert.Equal(t, "2", rr.Header().Get("X-RateLimit-Limit"))
 	assert.Equal(t, "0", rr.Header().Get("X-RateLimit-Remaining"))
-	
+
 }
